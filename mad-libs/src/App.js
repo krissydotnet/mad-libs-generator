0b5501aaf78075a6d021ba8661287e3984a49211
Krissy Dukes <krissydotnet@yahoo.com>
2019-01-09 06:43:20 -0500

    Day Two Progress
    Day Two Progress

--- mad-libs/src/App.js
+++ mad-libs/src/App.js
@@ -1,54 +1,16 @@
 import React, { Component } from 'react';
+import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
 import './App.css';
 import './styles/styles.css';
 
 import Words from './components/Words';
 import Display from './components/Display';
+import { stories, questions } from './data/data';
 
-const questions = {
-  'color' : 'Color',
-  'pluralnoun' : 'Plural Noun',
-  'adjective' : 'Adjective',
-  'exclamation' : 'Exclamation',
-  'sillyword' : 'Silly Word',
-  'verb_past' : 'Verb (Past Tense)',
-  'verb' : 'Verb',
-  'adverb' : 'Adverb',
-  'part-of-body' : 'Part of the body',
-  'part-of-body-plural':  'Part of the body (plural)',
-  'noun' : 'Noun',
-  'animal-plural': 'Animal (plural)',
-  'place': 'A place',
-  'number': 'Number'
-}
 
-const madLibText = [{ title: "Little Red Riding Hood", text: 
-`One day, Little {{color}} Riding Hood was going through the forest 
-carry a basket of {{pluralnoun}} for her grandmother.  Suddenly,
-she met a big {{adjective}} wolf. "{{exclamation}}!" said the wolf. 
-"Where are you going, little {{sillyword}}?" "I'm going to my
-grandmother's house,"  she said.  The the wolf {{verb_past}} away.
-When Miss Riding Hood got to her grandmother's house, the wolf was in bed dressed like her grandmother.  "My, Grandmother," she said.  "What big {{pluralnoun}} you have."  "The better to {{verb}} you with,"
-said the wolf.  "And, Grandmother," she said, "what big {{pluralnoun}} you have."  The wolf said, "The better to {{verb}} you with."  And then she said, "What big {{pluralnoun}} you have, Grandmother."  But the wolf said nothing.  He had just died of indigestion from eating Grandmother.
-`},
-{
-  title: "Wanted: Parent Chaperones",
-  text: `Are you daring, adventurous, and {{adjective}}?  Does the thought of driving in a stuffy, cramped {{noun}} with a bunch
-  of loud, boisterous {{pluralnoun}} make your {{part-of-body}} beat with excitement?  Can you picture yourself herding
-  rowdy, {{adjective}} children like a pack of pygmy {{animal-plural}}?  If so, then you could be a field trip chaperone!  Join
-  our team of ultra-responsible adult {{pluralnoun}} charged with making sure that students walk, not {{verb}}, around a zoo, a musuem, (the) {{place}}, or whatever location their {{adjective}} teacher has selected for the field trip.  While previous experience is not required, candidates who can manage up to {{number}} {{pluralnoun}} at any given time while maintaining a/an
-  {{adjective}} sense of humor are preferred.  Those with eyes in the back of their {{part-of-body-plural}} will be given top
-  consideration.  And while there's no salary, the joy of seeing {{adjective}} expressions on the kids' {{part-of-body-plural}} as
-  they learn something new will be reward enough!  If this sounds like a/an {{adjective}} job, {{verb}} today for an application!
-  `
-}
-];
+
+
 const placeholderRegEx = RegExp(/\{\{(.*?)\}\}/, 'gi');
-
-function replacePlaceholder(text) {
-
-  return text.replace(placeholderRegEx, '___________');
-}
 
 
 
@@ -66,9 +28,9 @@ class App extends Component {
    }
 
    loadStory = () => {
-     
-     let index = 1;
-     let story = madLibText[index];
+    
+     let index = Math.floor(Math.random() * stories.length);
+     let story = stories[index];
 
     this.setState({
       story: story,
@@ -113,14 +75,15 @@ class App extends Component {
   loadAnswers = () => {
 
     let index = 0;
-    return this.state.story.text.split(/[\s]/).map( (word) => {
+    return  this.state.story.text.split(/[\s]/).map( (word) => {
       if (placeholderRegEx.test(word)) {
-        return word.replace(placeholderRegEx, this.state.inputs[index++].answer);
+        return `<span class="highlight">${word.replace(placeholderRegEx, this.state.inputs[index++].answer)}</span>`;
       } else {
         return word;
       }
       
     }).join(' ');
+    
   }
       
 
@@ -134,9 +97,12 @@ class App extends Component {
       );
 
     } else {
+
+        const answers = ReactHtmlParser(this.loadAnswers());
+
       return (
         <div className="App">
-        <Display text={this.loadAnswers()} title={this.state.story.title}/> 
+        <Display text={answers} title={this.state.story.title}/> 
         </div>
       );
     }
---
+++ mad-libs/src/data/data.js
@@ -0,0 +1,49 @@
+export const stories = [{ title: "Little Red Riding Hood", text: 
+`One day, Little {{color}} Riding Hood was going through the forest 
+carry a basket of {{pluralnoun}} for her grandmother.  Suddenly,
+she met a big {{adjective}} wolf. "{{exclamation}}!" said the wolf. 
+"Where are you going, little {{sillyword}}?" "I'm going to my
+grandmother's house,"  she said.  The the wolf {{verb_past}} away.
+When Miss Riding Hood got to her grandmother's house, the wolf was in bed dressed like her grandmother.  "My, Grandmother," she said.  "What big {{pluralnoun}} you have."  "The better to {{verb}} you with,"
+said the wolf.  "And, Grandmother," she said, "what big {{pluralnoun}} you have."  The wolf said, "The better to {{verb}} you with."  And then she said, "What big {{pluralnoun}} you have, Grandmother."  But the wolf said nothing.  He had just died of indigestion from eating Grandmother.
+`},
+{
+  title: "Wanted: Parent Chaperones",
+  text: `Are you daring, adventurous, and {{adjective}}?  Does the thought of driving in a stuffy, cramped {{noun}} with a bunch
+  of loud, boisterous {{pluralnoun}} make your {{part-of-body}} beat with excitement?  Can you picture yourself herding
+  rowdy, {{adjective}} children like a pack of pygmy {{animal-plural}}?  If so, then you could be a field trip chaperone!  Join
+  our team of ultra-responsible adult {{pluralnoun}} charged with making sure that students walk, not {{verb}}, around a zoo, a musuem, (the) {{place}}, or whatever location their {{adjective}} teacher has selected for the field trip.  While previous experience is not required, candidates who can manage up to {{number}} {{pluralnoun}} at any given time while maintaining a/an
+  {{adjective}} sense of humor are preferred.  Those with eyes in the back of their {{part-of-body-plural}} will be given top
+  consideration.  And while there's no salary, the joy of seeing {{adjective}} expressions on the kids' {{part-of-body-plural}} as
+  they learn something new will be reward enough!  If this sounds like a/an {{adjective}} job, {{verb}} today for an application!
+  `
+},
+{
+    title: "Talk Like A Pirate",
+    text: `Ye can always pretend to be a bloodthirsy {{noun}}, threatening everyone by waving yer 
+    {{adjective}} sword in the air, but until ye learn to {{verb}} like a pirate, ye'll never be {{adverb}}
+    accepted as an authentic {{noun}}.  So here's what ye do: Cleverly work into yer daily conversation
+    {{adjective}} pirate phrases such as "Ahoy there, {{pluralnoun}}," "Avast, ye {{pluralnoun}}," and "Shiver me {{pluralnoun}}."  Remember to drop all yer gs when ye say such words as sailin',spitten', 
+    and fightin'.  This will give ye a/an {{part-of-body}} start to being recognized as a swashbucklin'
+    {{noun}}.  Once ye have the lingo down pat, it helps to wear a three-cornered {{noun}} on yer head,
+    stash a/an {{noun}} in yer pants, and keep a/an {{noun}} perched atop yer {{part-of-body}}.  Aye, now
+    ye be a real pirate!
+    `
+}
+];
+export const questions = {
+  'color' : 'Color',
+  'pluralnoun' : 'Plural Noun',
+  'adjective' : 'Adjective',
+  'exclamation' : 'Exclamation',
+  'sillyword' : 'Silly Word',
+  'verb_past' : 'Verb (Past Tense)',
+  'verb' : 'Verb',
+  'adverb' : 'Adverb',
+  'part-of-body' : 'Part of the body',
+  'part-of-body-plural':  'Part of the body (plural)',
+  'noun' : 'Noun',
+  'animal-plural': 'Animal (plural)',
+  'place': 'A place',
+  'number': 'Number'
+}
